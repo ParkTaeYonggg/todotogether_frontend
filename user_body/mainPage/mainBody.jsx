dea569bc63876b99640f1549b72c8a36da64adea
박태용 <wkddksehd333@naver.com>
2022-01-02 16:56:39 +0900

    메인페이지컨텐츠

--- user_body/mainPage/mainBody.jsx
+++ user_body/mainPage/mainBody.jsx
@@ -3,14 +3,16 @@ import styled from "styled-components";
 import "../userBody.scss";
 import Main1 from "./main1";
 import Main2 from "./main2";
+import Main3 from "./main3";
+import MainGoSignup from "./mainGoSignup";
 
 export default function MainBody () {
     return (
         <MainWrapper>
             <Main1 />
             <Main2 />
-            <h1>3</h1>
-            <h1>4</h1>
+            <Main3 />
+            <MainGoSignup />
         </MainWrapper>
     );
 }
@@ -19,7 +21,11 @@ const MainWrapper = styled.div`
     display: grid;
     grid-template-rows: 500px 400px 400px 100px;
     width: 100%;
-    row-gap: 40px;
+    row-gap: 100px;
     align-items: center;
+
+    @media screen and (max-width: 720px) {
+        row-gap: 40px;
+    }
 `;
 
---
+++ user_footer/footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Footer () {
+    return (
+        <FooterP>
+            ToDoTogether 푸터영역 푸터영역 푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역
+            푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역 
+            푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역 
+            푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역  푸터영역 푸터영역 
+        </FooterP>
+    );
+}
+
+const FooterP = styled.p`
+    margin: 70px 0px 40px 0px;
+`;
