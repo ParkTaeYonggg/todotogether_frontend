645b463c73bb1c94c1a6b3978787f39895642f43
박태용 <wkddksehd333@naver.com>
2022-01-02 16:12:23 +0900

    메인2컷

--- user_body/mainPage/mainBody.jsx
+++ user_body/mainPage/mainBody.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import styled from "styled-components";
 import "../userBody.scss";
 import Main1 from "./main1";
+import Main2 from "./main2";
+
 export default function MainBody () {
     return (
         <MainWrapper>
             <Main1 />
-            <h1>2</h1>
+            <Main2 />
             <h1>3</h1>
             <h1>4</h1>
         </MainWrapper>
@@ -17,7 +19,7 @@ const MainWrapper = styled.div`
     display: grid;
     grid-template-rows: 500px 400px 400px 100px;
     width: 100%;
-    gap-bottom: 20px;
+    row-gap: 40px;
     align-items: center;
 `;
 
